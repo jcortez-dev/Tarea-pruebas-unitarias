adebd7c264aeda28e1cce3a7ea10392c121aaea2
Jorge Cortez <j.cortez01@ufromail.cl>
2023-10-17 19:42:03 -0300

    feat: se agrega unit test de juegos por genero

--- tests/script.test.js
+++ tests/script.test.js
@@ -67,7 +67,11 @@ describe('Script tests', () => {
 
   describe('getGamesByGenre', () => {
     it('Should return a list of games by Action genre', () => {
-      expect(0).toBeTruthy();
+      const games = getTwoByGenre('Action');
+      
+      games.forEach((game) => {
+        expect(game.genres).toContain('Action');
+      })
     });
     it('Should return a empty list when the genre doesnt exists', () => {
       expect(0).toBeTruthy();
